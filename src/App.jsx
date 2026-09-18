a2cedb9158cdf9afd9136ea4079c4f9e8437590c
Khushi Shah <khushishah0717@gmail.com>
2026-09-18 16:58:57 +0530

    Fix page scroll position on navigation

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { useEffect } from "react";
+import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom";
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 
@@ -14,9 +15,22 @@ import SigninPage from './pages/SigninPage';
 import Dashboard from './pages/Dashboard';
 import ProtectedRoute from './components/ProtectedRoute';
 
+function ScrollToTop() {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+
+  return null;
+}
+
 function App() {
   return (
-    <Router>
+    <BrowserRouter>
+
+      <ScrollToTop />
+
       <div className="min-h-screen bg-[#0A0A0C] text-[#F8FAFC] flex flex-col justify-between">
 
         <Navbar />
@@ -75,7 +89,7 @@ function App() {
         <Footer />
 
       </div>
-    </Router>
+    </BrowserRouter>
   );
 }
 
